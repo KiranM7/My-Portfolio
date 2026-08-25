531b1235cd594512ad195d8bf0aade1fbb006e24
KiranM7 <kiranmahadik931@gmail.com>
2026-08-25 15:28:19 +0530

    update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 window.onload =() => {
     document.querySelector('.hero').style.opacity = 0;
     setTimeout(() => {
-        document.querySelector('.hero').style.transition = "0.5s";
+        document.querySelector('.hero').style.transition = "0.3s";
         document.querySelector('.hero').style.opacity = 1;
     }, 200);
 };
